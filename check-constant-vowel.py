bb071872ab71f6cf90d1fc41bc0ddc04da298819
VishalMakwna08 <makwanavishal588@gmail.com>
2025-08-06 20:15:50 +0530

    python program saved

---
+++ check-constant-vowel.py
@@ -0,0 +1,8 @@
+# Check if a character is a vowel or consonant
+
+character=input("Enter Any One Character:")
+
+if character in "aeiouAEIOU":
+    print(f"\'{character}\' Is Vowel Character")
+else:
+    print(f"\'{character}\' Is Constant Character")
