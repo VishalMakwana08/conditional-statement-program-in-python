508e0dba6c974b4fec43c002853fd124d239a682
VishalMakwna08 <makwanavishal588@gmail.com>
2025-08-06 19:52:09 +0530

    python program saved

---
+++ check-positive-negative.py
@@ -0,0 +1,12 @@
+#Check if a number is positive or negative.
+
+#Get Number From The User
+
+num=int(input("Enter Any Integer Number:"))
+
+if num<0:
+    print("You Enter Negative Number")
+elif num>0:
+    print("You Enter Positive Number")
+else:
+    print("You Enter Zero")
