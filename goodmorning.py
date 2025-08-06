609871b7ffac1157bd941dc7ec852a382e31d359
VishalMakwna08 <makwanavishal588@gmail.com>
2025-08-06 20:22:16 +0530

    python program saved

---
+++ goodmorning.py
@@ -0,0 +1,8 @@
+# Print "Good Morning" if time is less than 12.
+
+#Get Time From The User
+
+time=int(input("Enter Hour Betweent 1-24:"))
+
+if time<12 and time>0 and time<=24:
+    print("Good Morning")
